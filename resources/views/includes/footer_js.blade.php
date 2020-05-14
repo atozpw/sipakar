be40df6847c2ea09e29eb564262158492057a9fe
rezhasdf <rezhasdf@gmail.com>
2020-05-14 21:46:02 +0700

    Optimizing components

--- resources/views/includes/footer_js.blade.php
+++ resources/views/includes/footer_js.blade.php
@@ -4,5 +4,4 @@
 <script src="{{ asset('js/popper.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('js/sweetalert.min.js') }}"></script>
-<script src="{{ asset('js/main.js') }}"></script>
 @stack('scriptku')
