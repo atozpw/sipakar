153f5bef70cf518f7e5c48004f5753479110303d
rezhasdf <rezhasdf@gmail.com>
2020-05-14 20:47:37 +0700

    Adjusting tanaman CRUD

--- resources/views/tanaman/ajax/action.blade.php
+++ resources/views/tanaman/ajax/action.blade.php
@@ -1,2 +1,2 @@
-<button type="button" data-toggle="modal" data-target="#ubahDaerahGejala" class="btn btn-sm btn-primary" data-id="{{ $data->id }}">Ubah</button>
-<button type="button" class="btn btn-sm btn-danger" onclick="hapusDaerahGejala({{ $data->id }})">Hapus</button>
+<button type="button" class="btn btn-sm btn-primary" onclick="showEditModal({{ $data->id }})" data-id="{{ $data->id }}">Ubah</button>
+<button type="button" class="btn btn-sm btn-danger" onclick="hapusTanaman({{ $data->id }})">Hapus</button>
